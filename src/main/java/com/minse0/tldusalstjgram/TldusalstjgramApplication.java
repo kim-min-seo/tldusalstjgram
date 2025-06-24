4e8f8c58a8a380a81cd12d40ef4751b310ae8a40
minse0 <3058ms@naver.com>
2025-06-24 13:07:41 +0900

    [김민서] springPorject 초기설정

---
+++ src/main/java/com/minse0/tldusalstjgram/TldusalstjgramApplication.java
@@ -0,0 +1,13 @@
+package com.minse0.tldusalstjgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TldusalstjgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TldusalstjgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minse0/tldusalstjgram/TldusalstjgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minse0.tldusalstjgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TldusalstjgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
