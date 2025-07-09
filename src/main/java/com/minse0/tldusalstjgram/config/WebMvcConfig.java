0c545b29620a88c3ba67dc1c4d98dceff8197847
B3-13\User <3058ms@naver.com>
2025-07-09 17:43:14 +0900

    [김민서] - 로그인 여부 확인을 위한 PermissionInterceptor 추가

--- src/main/java/com/minse0/tldusalstjgram/config/WebMvcConfig.java
+++ src/main/java/com/minse0/tldusalstjgram/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.minse0.tldusalstjgram.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.minse0.tldusalstjgram.common.Filemanager;
+import com.minse0.tldusalstjgram.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -14,4 +16,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + Filemanager.FILE_UPLOAD_PATH + "/");
 	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new PermissionInterceptor())
+		.addPathPatterns("/**")
+		.excludePathPatterns("/user/logout");
+	}
+	
 }
